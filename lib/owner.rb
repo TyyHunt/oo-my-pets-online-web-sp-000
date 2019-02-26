735e142319893168403e10f11a7bb277141bd973
Tyler Hunt <tyhunt04@gmail.com>
2019-02-26 23:43:22 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,55 @@
 class Owner
-  # code goes here
+
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  @@all = []
+
+  def initialize(species)
+    @species = species
+    @@all << self
+    @pets = {:fishes => [], :cats => [], :dogs => []}
+  end
+
+  def say_species
+    return "I am a #{@species}."
+  end
+
+  def buy_fish(fish_name)
+    @pets[:fishes] << Fish.new(fish_name)
+  end
+
+  def buy_cat(cat_name)
+    @pets[:cats] << Cat.new(cat_name)
+  end
+
+  def buy_dog(dog_name)
+    @pets[:dogs] << Dog.new(dog_name)
+  end
+
+
+  def walk_dogs
+    Dog.mood = "happy"
+  end
+
+  def play_with_cats
+    Cat.mood = "happy"
+  end
+
+  def feed_fish
+    Fish.mood = "happy"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.count
+    @@all.size
+  end
+
 end
